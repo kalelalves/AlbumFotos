118188e39ead880a198e3ca0199cebfc549a2a51
Kalel Jamil Alves <73508900+kalelalves@users.noreply.github.com>
2022-03-15 18:05:39 -0300

    Adicionar arquivos de projeto.

---
+++ AlbumFotos/Migrations/20220315205724_criacaoDB.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlbumFotos.Migrations
+{
+    public partial class criacaoDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Link",
+                table: "Imagens",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FotoTopo",
+                table: "Albuns",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Link",
+                table: "Imagens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FotoTopo",
+                table: "Albuns",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
